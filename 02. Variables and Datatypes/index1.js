fe8f79f9d4cbe1c3e33f8d72f941def09dd41bfc
ch-sakshi <ch.sakshi005@gmail.com>
2025-12-29 23:22:08 +0530

    add: code for variables and datatypes

--- 02. Variables and Datatypes/index1.js
+++ 02. Variables and Datatypes/index1.js
@@ -1,15 +1,28 @@
-// How to create variables.
+//  How to create variables.
 
-let name ="Sakshi Tomar";
+// let name ="Sakshi Tomar";
 
-let age = 26;
-age = 27;
+// let age = 26;
+// age = 27;
+// console.log(name, age);
 
-console.log(name, age);
+// const account = 12345;
+//  account =34556; // const variables can not be redeclared, this line will throw error.
 
-const account = 12345;
-// account =34556; // const variables can not be redeclared, this line will throw error.
+// console.log(account);
 
-console.log(account);
+// old method to create variables
+// var a = 20;
+// var a = 30;
 
+// if (true){
+//     var a = 40;
+// }
+
+// function fun(){
+// var c = 20;
+// }
+
+// var b = 30;
+// console.log(c);
 
---
+++ 02. Variables and Datatypes/index2.js
@@ -0,0 +1,113 @@
+// Data Types
+
+// Primitive Data Type
+
+// Number, String, Boolean, Undefined, Null, Bigint, Symbol
+
+// Number
+
+// let a = 20; //integer value
+// let b = 23.45//float value means decimal number
+// console.log(a,b);
+// console.log(typeof a);// number
+
+// String
+
+// let c = "Strike is Coming";
+// let d = "Anjali";
+// console.log(c, d);
+// console.log(typeof c); // string
+
+// Boolean
+
+// let login = true;
+// let f = false;
+// console.log(login,f);
+// console.log(typeof login); // boolean
+
+// undefined
+
+// let user; // A variable that has been declared but not assigned a value is undefined.
+// const p = 10;// in the case of const we have to defined the value of const.
+// console.log(user,p);
+// console.log(typeof user); // undefined
+
+// Bigint
+// let num = 123456789785489474n;
+// console.log(num);
+// console.log(typeof num); //bigint
+
+// null
+
+// let weather = null;
+// console.log(weather);
+// console.log(typeof weather); //object
+
+// let weather = current_weather(meerut);
+//25
+// null
+//undefined
+
+// symbol
+
+// const id1 = Symbol("hello");
+// const id2 = Symbol("hello");
+// console.log(id2==id1);// id1 is not equal to id2. Every symbol is unique.
+// console.log(typeof id1); //symbol
+
+// Non Primitive Data Type
+
+// Array, Object, Function.
+
+// Array
+
+// let arr = [10, 20, 50,"Suryanshu","Sakshi",true,false];
+// console.log(arr);
+// console.log(typeof arr);//object
+
+// Object
+
+//Sakshi 12345 27 obc
+
+// let user = {
+//   name: "Sakshi",
+//   account: 12345,
+//   age: 27,
+//   category: "Obc",
+// };
+// console.log(typeof user);
+// Function
+
+// let s = function add(){
+//  console.log("Hello");
+//  }
+// s();// In Js function can be stored in variables.
+// console.log(typeof s);
+
+// Primitive data type is immutable. (immutable means which value we can not change)
+
+// let a = 10;
+// let b = a;
+//  b = 20;
+
+//  console.log(a,b);
+
+// let str = "Sakshi";
+// str = "Tomar";
+
+// console.log(str);
+
+// Non Primitive data type mutable. (mutable means which value we can change)
+
+let arr = [10, 20, 30, 40, 49];
+arr.push(90);
+arr[0] = 50;
+console.log(arr);
+
+let obj = {
+  name: "Sakshi",
+  age: 27,
+};
+let obj2 = obj;
+obj.name = "Tanu";
+console.log(obj);

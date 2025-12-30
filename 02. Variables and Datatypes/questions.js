9eea6a139ea439affa06f7c0ecbcfd95a8fa396c
ch-sakshi <ch.sakshi005@gmail.com>
2025-12-30 21:56:29 +0530

    add: questions file

---
+++ 02. Variables and Datatypes/questions.js
@@ -0,0 +1,55 @@
+// Q1: How do you declare a variable in JavaScript?
+
+// Awswer: There are 3 methods to declare variables in JavaScript.
+// 1. var 
+// var fullName = "sakshi tomar";
+
+// 2.Let
+// let age = 27;
+
+// 3. const
+// const account = 12345;
+                                                                                                           
+// Q2: What is the difference between var, let, and const?
+
+// Awswer: var: Variables can be re-declared and updated.var is a global scope variable.
+
+// let: Variables can be re-declared but can be updated. It is a block scope variable.
+
+// const: Variables cannot be re-declared or updated. It is a block scope variable.
+
+// Q3: Can you change the value of a const variable?
+// Awswer: NO Const variables can not be redeclared, this line will throw error.
+
+// Q4: What will happen if you use a variable without declaring it?
+// Awswer: It will throw ReferenceError.
+
+// Q5: What is the default value of an uninitialized variable in JavaScript?
+// Awswer: Default value is undefined.
+
+// Q6: What are the primitive data types in JavaScript?
+// Awswer: There are 7 primitive data types in JavaScript.
+// 1. Number
+// 2. String
+// 3. Boolean
+// 4. Undefined
+// 5. Null
+// 6. Bigint
+// 7. Symbol 
+
+// Q7: What is the difference between null and undefined?
+// Answer: Null: A variables that had been declared but we assigned intialized value.
+// Undefined: A variables that has been declared but not assigned intialized value.
+
+// Q8: Is JavaScript a statically typed or dynamically typed language?
+// Answer: Dynamically Type
+
+// Q9: What will be the output of typeof null?
+// Awswer: Object
+
+// Q10: What happens when you add a number and a string in JavaScript?
+// Q11: What is type coercion? Give an example.
+// Q12: How can you manually convert a string to a number in JavaScript?
+// Q13: What is the result of "5" - 3 in JavaScript?
+// Q14: What is NaN in JavaScript, and how do you check if a value is NaN?
+// Q15: How do you check the type of a variable in JavaScript?
